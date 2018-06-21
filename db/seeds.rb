d2306ae9225490477cc323efcd5adcdd75170cb7
gjbonner <gjbonner@gmail.com>
2018-06-21 19:07:39 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@ require_relative '../app/models/chore'
 require_relative '../app/models/roommate'
 
 giles = Roommate.create(name: "Giles")
-laundry = Chore.create(chore: "laundry")
+laundry = Chore.create(chore: "Laundry")
 chore_1 = AssignedChore.create(roommate: giles, chore: laundry)
